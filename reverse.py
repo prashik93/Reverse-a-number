a269408283a271a3fdd8db7ad2903dc32eb43caa
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-04 07:52:58 +0530

    [prashik]changes in code

--- reverse.py
+++ reverse.py
@@ -1,10 +1,16 @@
-num=1234
+num=12345
 temp=num
-rev=0
+rev =0
+count=0
 
 while num>0:
     rem=num%10
     rev=(rev*10)+rem
     num=num//10
+    count+=1
 
-print (rev)
+print(rev)
+print(count)
+
+same_position=(count+1)/2
+print(same_position)
